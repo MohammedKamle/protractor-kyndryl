9d5972a8772390273489fe285f859fd668817146
Mohammed Kamle <mohammed@Mohammeds-MacBook-Pro.local>
2021-10-12 20:51:43 +0530

    initial commit

---
+++ conf.js
@@ -0,0 +1,28 @@
+// An example configuration file.
+exports.config = {
+    directConnect: false,
+
+    'browserstackUser': process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
+    'browserstackKey': process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY',
+
+    // Capabilities to be passed to the webdriver instance.
+    'capabilities': {
+        'build': process.env.BROWSERSTACK_BUILD_NAME,
+        'name': 'single_test',
+        'browserName': 'chrome',
+        'resolution': '1024x768',
+        'browserstack.debug': 'true'
+    },
+
+    // Framework to use. Jasmine is recommended.
+    framework: 'jasmine',
+
+    // Spec patterns are relative to the current working directory when
+    // protractor is called.
+    specs: ['example_spec.js'],
+
+    // Options to be passed to Jasmine.
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
+    }
+};
